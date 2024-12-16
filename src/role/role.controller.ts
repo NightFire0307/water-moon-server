2d9cb6e1664d18c52912e13dc087cab421ea7517
繁花落叶已成霜 <1035612457@qq.com>
2024-12-16 22:45:24 +0800

    feat: 新增角色创建和获取接口

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { RoleService } from './role.service';
+import {
+  Pagination,
+  PaginationQuery,
+  RequireLogin,
+} from '../common/custom.decorator';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Controller('admin/roles')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Get()
-  async getRoles() {
-    return this.roleService.getRoles();
+  @RequireLogin()
+  async getRoles(@Pagination() pagination: PaginationQuery) {
+    return this.roleService.getRoles(pagination);
   }
 
   @Post()
-  async createRole() {
-    return this.roleService.createRole();
+  @RequireLogin()
+  async createRole(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.createRole(createRoleDto);
   }
 }
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from '../auth/entities/role.entity';
+import { Repository } from 'typeorm';
+import { PaginationQuery } from '../common/custom.decorator';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Injectable()
 export class RoleService {
-  getRoles() {
-    return 'Get roles';
+  @InjectRepository(Role)
+  private roleRepository: Repository<Role>;
+
+  async getRoles(pagination: PaginationQuery) {
+    const [data, total] = await this.roleRepository.findAndCount({
+      skip: (pagination.current - 1) * pagination.pageSize,
+      take: pagination.pageSize,
+    });
+    return {
+      data,
+      total,
+      pageSize: pagination.pageSize,
+      current: pagination.current,
+    };
   }
 
-  createRole() {
-    return 'Create role';
+  async createRole(createRoleDto: CreateRoleDto) {
+    const foundRole = await this.roleRepository.findOneBy({
+      name: createRoleDto.name,
+    });
+
+    if (foundRole) return '该角色已存在';
+
+    await this.roleRepository.save(createRoleDto);
+    return '创建成功';
   }
 }
