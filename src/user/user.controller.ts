b8860bd5f44184671f8900a37422e5b4010fabb1
繁花落叶已成霜 <1035612457@qq.com>
2024-12-17 16:15:48 +0800

    添加接口权限标识和描述

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,29 +13,45 @@ import {
   Pagination,
   PaginationQuery,
   RequireLogin,
+  RequirePermission,
   UserInfo,
 } from '../common/custom.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
 import { UserDetailVo } from './vo/user-detail.vo';
 import { UpdateUseDto } from './dto/update-use.dto';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('用户管理模块')
+@ApiBearerAuth()
 @Controller('admin/users')
 export class UserController {
-  constructor(private readonly adminService: UserService) { }
+  constructor(private readonly adminService: UserService) {}
 
   @Get()
   @RequireLogin()
+  @ApiQuery({
+    name: 'username',
+    required: false,
+    description: '用户名',
+  })
+  @ApiQuery({
+    name: 'nickname',
+    required: false,
+    description: '昵称',
+  })
+  @RequirePermission('user_view', '查看用户列表')
   async findAllUsers(
-    @Query('username') username: string,
-    @Query('nickname') nickname: string,
     @Pagination() pagination: PaginationQuery,
+    @Query('username') username?: string,
+    @Query('nickname') nickname?: string,
   ) {
     return await this.adminService.findAllUsers(username, nickname, pagination);
   }
 
   @Get('info')
   @RequireLogin()
+  @RequirePermission('user_view_detail', '查看用户信息')
   async findUserById(@UserInfo('userId') userId: number) {
     const user = await this.adminService.findUserDetailById(userId);
     const vo = new UserDetailVo();
@@ -52,12 +68,14 @@ export class UserController {
 
   @Post()
   @RequireLogin()
+  @RequirePermission('user_create', '创建用户')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.adminService.createUser(createUserDto);
   }
 
   @Put(':id')
   @RequireLogin()
+  @RequirePermission('user_update', '更新用户信息')
   async updateUser(
     @Param('id') userId: string,
     @Body() updateUserDto: UpdateUseDto,
@@ -68,12 +86,14 @@ export class UserController {
 
   @Delete(':id')
   @RequireLogin()
+  @RequirePermission('user_delete', '删除用户')
   async removeUser(@Param('id') userId: string) {
     return this.adminService.deleteUser(parseInt(userId));
   }
 
   @Post('update_password')
   @RequireLogin()
+  @RequirePermission('user_update', '更新用户密码')
   async updatePassword(
     @UserInfo('userId') userId: number,
     @Body() passwordDto: UpdateUserPasswordDto,
