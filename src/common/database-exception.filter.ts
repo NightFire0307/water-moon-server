22bd1025d43f404bc20b37c717caafeafda97dd9
繁花落叶已成霜 <1035612457@qq.com>
2025-01-03 17:09:49 +0800

    返回字段更新

--- src/common/database-exception.filter.ts
+++ src/common/database-exception.filter.ts
@@ -25,21 +25,21 @@ export class DatabaseExceptionFilter implements ExceptionFilter {
       case '数据已存在':
         return response.json({
           code: HttpStatus.CONFLICT,
-          message: exception.message,
+          msg: exception.message,
           data: '',
         });
 
       case '数据不存在':
         return response.json({
           code: HttpStatus.NOT_FOUND,
-          message: exception.message,
+          msg: exception.message,
           data: '',
         });
 
       default:
         return response.json({
           code: HttpStatus.BAD_REQUEST,
-          message: '数据库操作失败',
+          msg: '数据库操作失败',
           data: exception.message || '数据库操作失败',
         });
     }
